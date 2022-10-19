d1976f25775aef9e2c7f4b4778066ddccf1366b3
Chroma7p <nahco3.hcl@gmail.com>
2022-10-19 23:19:15 +0900

    IDが見つからなかった時の対応、10日切り取りを追加

--- front/qsahaiel/pages/index.tsx
+++ front/qsahaiel/pages/index.tsx
@@ -1,30 +1,47 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import { useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
 import { axios } from "../util/axios";
 
+
 const Home: NextPage = () => {
   const inputEl = useRef(null);
   const [text, setText] = useState(<></>);
+  const [grass, setGrass] = useState([]);
 
-  const handleClick = () => {
+  const searchID = () => {
     if (!inputEl.current?.value) return;
-    axios.get("/grass/" + inputEl.current.value).then((res) => {
-      setText(
-        <>
-          {res.data.map((date: [string, string]) => {
-            return (
-              <>
-                <p key={date[0]}>{date[0] + " : " + date[1]}</p>
-              </>
-            );
-          })}
-        </>
-      );
-      console.log(res.data);
-    });
+    axios
+      .get("/grass/" + inputEl.current.value)
+      .then((res) => {
+        if (res.status != 200) {
+          setGrass([]);
+          setText(<>ユーザーが見つかりませんでした</>);
+        } else {
+          setGrass(res.data);
+          setText(
+            <>
+              {res.data.slice(-10).map((date: [string, string]) => {
+                return (
+                  <>
+                    <p key={date[0]}>{date[0] + " : " + date[1]}</p>
+                  </>
+                );
+              })}
+            </>
+          );
+        }
+        console.log(res.data);
+      })
+      .catch((res) => {
+        setGrass([]);
+        setText(<>ユーザーが見つかりませんでした</>);
+      });
+  };
+
+  const sendGrass = () => {
+    //ここに草の情報を送信する処理
   };
 
   return (
@@ -34,8 +51,15 @@ const Home: NextPage = () => {
       </Head>
 
       <input type="text" id="userID" required size={20} ref={inputEl}></input>
-      <button onClick={handleClick}>検索</button>
+      <button onClick={searchID}>検索</button>
       <p>{text}</p>
+      <>
+        {grass.length != 0 ? (
+          <button onClick={sendGrass}>ここを押したら草の情報を送信する</button>
+        ) : (
+          <></>
+        )}
+      </>
     </div>
   );
 };
