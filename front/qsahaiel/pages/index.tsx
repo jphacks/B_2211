ef895ba837c151091e289460dec5f1ddf0432f4a
Chroma7p <nahco3.hcl@gmail.com>
2022-10-20 20:17:45 +0900

    UIの改善

--- front/qsahaiel/pages/index.tsx
+++ front/qsahaiel/pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useRef, useState } from "react";
-import styles from "../styles/Home.module.css";
 import { axios } from "../util/axios";
 
 const Home: NextPage = () => {
@@ -51,9 +50,9 @@ const Home: NextPage = () => {
     //草の情報を送信する
 
     //草の情報がないケース(ボタンが表示されないはずなので多分ない)
-    if(grass.length==0){
-      setSent(<>送信できません</>)
-      return 
+    if (grass.length == 0) {
+      setSent(<>送信できません</>);
+      return;
     }
 
     //草の情報をAPI用に整形
@@ -61,7 +60,6 @@ const Home: NextPage = () => {
       const pow = Number(date[1]);
       return { color: "#0000FF", power: pow };
     });
-
 
     //送信
     axios
@@ -75,22 +73,45 @@ const Home: NextPage = () => {
   };
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Qsahaiel Top page</title>
       </Head>
 
-      <input type="text" id="userID" required size={20} ref={inputEl}></input>
-      <button onClick={searchID}>検索</button>
-      <p>{text}</p>
-      <>
-        {grass.length != 0 ? (
-          <button onClick={sendGrass}>草の情報を送信</button>
-        ) : (
-          <></>
-        )}
-      </>
-      <>{sent}</>
+      <div className="h-screen flex flex-col align-middle">
+        <h1 className=" my-10 text-7xl text-center font-mono">Qsahaiel</h1>
+
+        <div className="justify-center  flex flex-row">
+          <input
+            type="text"
+            id="userID"
+            required
+            size={20}
+            ref={inputEl}
+            className=" block p-4 w-3/5 text-gray-900 bg-gray-50 rounded-l-lg border border-gray-300 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          ></input>
+          <button
+            onClick={searchID}
+            className="bg-green-700 hover:bg-green-600 text-white rounded-r-lg px-8 py-2"
+          >
+            検索
+          </button>
+        </div>
+        <p className="text-center my-3">{text}</p>
+        <div className="text-center">
+          {grass.length != 0 ? (
+            <button
+              onClick={sendGrass}
+              className="bg-green-700 hover:bg-green-600 text-white rounded-lg px-4 py-2 w-48 "
+            >
+              草の情報を送信
+            </button>
+          ) : (
+            <></>
+          )}
+        </div>
+        <>{sent}</>
+      </div>
     </div>
   );
 };
