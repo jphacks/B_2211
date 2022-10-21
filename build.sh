46420a25512053e12e2bc42e7e83b233aeea2f73
VOID <kouichi.hirachi@gmail.com>
2022-10-21 13:02:34 +0900

    fixed registory

--- build.sh
+++
@@ -1,3 +0,0 @@
-docker build -t ghcr.io/csenet/front front
-docker build -t ghcr.io/csenet/back back
-docker build -t ghcr.io/csenet/state-sync state-sync
---
+++ push.sh
@@ -0,0 +1,6 @@
+docker tag b2211_front ghcr.io/csenet/qusahaiel/front:latest
+docker tag b2211_kusa-api ghcr.io/csenet/qusahaiel/kusa-api:latest
+docker tag b2211_state-sync ghcr.io/csenet/qusahaiel/state-sync:latest
+docker push ghcr.io/csenet/qusahaiel/front:latest
+docker push ghcr.io/csenet/qusahaiel/kusa-api:latest
+docker push ghcr.io/csenet/qusahaiel/state-sync:latest
